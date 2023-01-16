01b6efe90118fd4668ac31025ce80a7ee189acdf
Mathis95c <114164132+Mathis95c@users.noreply.github.com>
2023-01-16 19:28:34 +0100

    Add files via upload

--- project/src/model/Menu.java
+++ project/src/model/Menu.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 import model.Joueur;
@@ -9,6 +10,8 @@ public class Menu {
     static String nom;
     static String couleur;
     public static Joueur joueur01 = new Joueur();
+    public static Joueur joueur02 = new Joueur();
+    public static Joueur joueur03 = new Joueur();
 
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_RED = "\u001B[31m";
@@ -24,11 +27,10 @@ public class Menu {
             String choix = scan01.nextLine();
             switch (choix) {
                 case "1":
-                    menuSecondaire();
-                    System.out.println(joueur01.couleurJoueur + joueur01.nomJoueur);
+                    menuSecondaireSolo();
                     break;
                 case "2":
-                    System.out.println("Jouer à 2");
+                    menuSecondaireDuo();
                     break;
                 case "3":
                     System.out.println("Afficher le TOP 10");
@@ -53,7 +55,7 @@ public class Menu {
         menus.add(ANSI_YELLOW + "3 ➥ Afficher le TOP 10" + ANSI_RESET);
 
         menus.add(ANSI_RED + "\nq ➥ Quitter" + ANSI_RESET);
-        menus.add(ANSI_YELLOW + "\n⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺\n" + ANSI_RESET);
+        menus.add(ANSI_YELLOW + "\n⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺\n" + ANSI_RESET);
         for (String s : menus) {
             System.out.println(s);
         }
@@ -61,17 +63,40 @@ public class Menu {
 
 
 
-    private static void menuSecondaire(){
+    private static void menuSecondaireSolo(){
         demanderNom();
         menuCouleur();
         joueur01.nomJoueur = Menu.nom;
         joueur01.couleurJoueur = Menu.couleur;
+        System.out.println("Bienvenue "+joueur01.nomJoueur+ " ! Votre couleur est "+joueur01.couleurJoueur+".");
         return;
 
     }
 
+    private static void menuSecondaireDuo(){
+        System.out.println("\n--- Saissisez les informations du joueur 1 ---\n");
+        demanderNom();
+        menuCouleur();
+        joueur02.nomJoueur = Menu.nom;
+        joueur02.couleurJoueur = Menu.couleur;
+        System.out.println("Bienvenue "+joueur02.nomJoueur+ " ! Votre couleur est "+joueur02.couleurJoueur+".");
+        System.out.println("\n--- Saissisez les informations du joueur 2 ---\n");
+        demanderNom();
+        choixCouleurDuo();
+        joueur03.nomJoueur = Menu.nom;
+        System.out.println("Bienvenue "+joueur03.nomJoueur+ " ! Votre couleur est donc le "+joueur03.couleurJoueur+".");
+
+    }
+
+    public static void choixCouleurDuo(){
+        if (Menu.couleur == "🟡"){
+            joueur03.couleurJoueur = "🔴";
+        }else{
+            joueur03.couleurJoueur = "🟡";
+        }
+    }
     public static void demanderNom() {
-        System.out.println("Saissisez le nom du joueur :");
+        System.out.println(">> Saissisez le nom du joueur :");
         Menu.nom = scan03.nextLine();
     }
 
@@ -96,10 +121,9 @@ public class Menu {
         }
     }
 
-
     private static void afficherMenuCouleur() {
         ArrayList<String> menus2 = new ArrayList<>();
-        menus2.add("\nChoissisez votre couleur :\n");
+        menus2.add("\n>> Choissisez votre couleur :\n");
         menus2.add(ANSI_YELLOW + "1 ➥ 🟡" + ANSI_RESET);
         menus2.add(ANSI_YELLOW + "2 ➥ 🔴" + ANSI_RESET);
 
@@ -108,3 +132,4 @@ public class Menu {
         }
     }
 }
+
